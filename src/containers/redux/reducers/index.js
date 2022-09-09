05370aa95edf1f11f2c9179899a12f88fc448b44
Nghiattk27 <immomvao@gmail.com>
2022-09-09 15:42:07 +0700

    first

---
+++ src/containers/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import { productReducer, seletedProductReducer } from './productReducer';
+
+export const reducers = combineReducers({
+    allProducts: productReducer,
+    product: seletedProductReducer,
+})
+
+
